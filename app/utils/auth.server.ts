f8c8cf9ce0990ad2d398bf3d0ee21424e8d2743a
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-24 19:47:15 +0530

    config: Create an instance of PrismaClient

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,3 +1,13 @@
+import { json } from "@remix-run/node";
+import { prisma } from "./prisma.server";
 import type { RegisterForm } from "./types.server";
 
-export async function register(user: RegisterForm) {}
+export async function register(user: RegisterForm) {
+  const exists = await prisma.user.count({ where: { email: user.email } });
+  if (exists) {
+    return json(
+      { error: `User already exists with that email` },
+      { status: 400 }
+    );
+  }
+}
